ac98f5b5cd690114b6864adc85ca390abeaf1ea1
dhananjay12 <dhananjay1204@gmail.com>
2020-04-13 17:03:13 +0200

    DB release

--- src/main/java/com/djcodes/spring/demoapp/controller/FortuneController.java
+++ src/main/java/com/djcodes/spring/demoapp/controller/FortuneController.java
@@ -1,6 +1,7 @@
 package com.djcodes.spring.demoapp.controller;
 
 import com.djcodes.spring.demoapp.service.FortuneService;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +16,10 @@ public class FortuneController {
     private final FortuneService fortuneService;
 
     @GetMapping("/fortune")
-    public ResponseEntity<String> getFortune() {
+    public ResponseEntity<Map<String, String>> getFortune() {
         String fortune = fortuneService.getFortune();
-        return ResponseEntity.ok(fortune);
+        Map<String,String> result= Map.of("fortune", fortune);
+        return ResponseEntity.ok(result);
     }
 
 }
